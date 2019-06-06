8f1497b2ced56db1f2bfd17db07d542faabde757
joe-alfaro <josephlalfaro@gmail.com>
2019-06-05 18:50:57 -0700

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const moveSelected = () => {
+  //Do you dare take this on? Maybe look into JQuery, 
+  //it's not taught at Lambda since it has nothing to do with React 
+  //but it's still alive out there since the internet was built on it for a long time. 
+  //Good luck.
+}
